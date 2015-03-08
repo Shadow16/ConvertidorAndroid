3d4768a7faeac4777e28d293ed627f50cd72fd70
Shadow16 <nestor_infante@hotmail.com>
2015-03-08 10:00:03 -0600

    commit

---
+++ app/build/generated/source/buildConfig/debug/com/cutonala/calculadora/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.cutonala.calculadora;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.cutonala.calculadora";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "";
+}
